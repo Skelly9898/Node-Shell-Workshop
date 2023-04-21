b9a10e75e2e84eae18123d340080a2c4b24bc916
Steven <ksteve998@gmail.com>
2023-04-21 15:07:25 -0500

    swap partners

--- node-shell/bash.js
+++ node-shell/bash.js
@@ -1,1 +1,11 @@
-//test
+//Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim(); //remove the newline
+
+  process.stdout.write('You typed: ' + cmd);
+  process.stdout.write(process.cwd())
+  process.stdout.write('\nprompt > ');
+});
